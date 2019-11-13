b5250934b3e15c3b4ea40fc9ee23ad3d989735de
Ben Tsardoulias <38056175+TIBTHINK@users.noreply.github.com>
2019-11-13 12:01:10 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+import math
+from math import pi
+import random
+
+a = input("are you sure you wanna do this?: ")
+
+if "yes" in a:
+        while True:
+            sec = random.randint(44,561467984602508721666)
+            fir = random.randint(1,13461531269420)
+            thir = fir*sec
+            end = thir/65
+            print(end)
+
+else:
+    exit()
